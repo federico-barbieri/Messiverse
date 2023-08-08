b23bf0aa7f4778bf02353bfcb030fe87ffdd8e4f
Federico Barbieri <federicobarbieri54@gmail.com>
2023-08-08 23:28:30 +0200

    cart

---
+++ src/components/Cart.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <span>CART</span>
+        <ul>
+            <li>thing 1</li>
+            <li>thing 2</li>
+        </ul>
+        <span>X</span>
+    </div>
+    
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+div{
+    width: 40rem;
+    height: 40rem;
+    background-color: red;
+    border-radius: 10px;
+    margin: 10rem auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    color: black;
+    font-size: 2rem;
+}
+
+</style>
--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,36 +1,32 @@
 <template>
-    <header>
+  <header>
     <nav>
-
       <router-link to="/">HOME</router-link>
       <router-link to="/football">FOOTBALL</router-link>
-
-
       <router-link to="/about">ABOUT</router-link>
-
       <span>CART({{ cartStore.getCount }})</span>
-
+      <button @click="showMeTheCart">show cart</button>
     </nav>
-
-  
-   
   </header>
-<main>
-
-    <slot/>
-
-</main>
- 
+  <main>
+    <Cart v-show="showCart.value" />
+    <slot />
+  </main>
 </template>
 
 <script setup>
-
 import { useCartStore } from '../stores/CartStore';
+import Cart from "./Cart.vue"
+import { ref } from 'vue';
 
 const cartStore = useCartStore();
+let showCart = ref(false); // Use the ref function to create a reactive property
 
+function showMeTheCart(){
+  showCart.value = true;
+}
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+/* Your styles here */
 </style>
