fd46325bf94c94484a7fca375581d7164778c200
Federico Barbieri <federicobarbieri54@gmail.com>
2023-10-15 22:26:30 +0200

    michelangelo

--- src/views/ArtistPages/Michelangelo.vue
+++ src/views/ArtistPages/Michelangelo.vue
@@ -1,10 +1,10 @@
 <template>
     <main class="michelangelo-main">
           <div class="main-title">
-                    <h1>MESSI IN MICHELANGELO'S UNIVERSE</h1>
+                    <h1 id="mainTitle">MESSI IN MICHELANGELO'S UNIVERSE</h1>
           </div>
 
-          <div class="gallery">
+          <div class="grid">
                 <ul>
                       <li v-for="index in michelangelo.image.length" :key="index" class="img-span-li">
                           <div class="img-wrapper" :style="{ order: index % 2 === 0 ? '2' : '1' }">
@@ -17,6 +17,8 @@
                       </li>
                   </ul>
             </div>
+            <a href="#mainTitle">Back to top</a>
+
      </main>
 </template>
 
@@ -32,28 +34,23 @@ const michelangelo = artists.find((artist) => artist.name === "Michelangelo");
 <style scoped>
 
 .main-title{
-    width: 100vw;
-    height: 90vh;
+  width: 100vw;
+    height: 80vh;
+    border: 1px solid white;
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: space-around;
-    margin: 0rem auto;
+    justify-content: center;
 }
 
 h1{
-    font-size: 4rem;
-    display: block;
-    margin: 0 auto;
-    padding: 1rem;
-    text-align: center;
+    font-size: 3rem;
 }
 
-/* gallery */
-
-.gallery{
-    max-width: 90vw;
+.grid{
+    width: 80vw;
     height: auto;
+    border: 1px solid white;
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -62,8 +59,8 @@ h1{
 }
 
 
-main > ul{
-    max-width: 90%;
+.grid > ul{
+    width: 100%;
     height: auto;
     list-style-type: none;
     display: flex;
@@ -74,22 +71,16 @@ main > ul{
     padding: 1rem;
 }
 
-ul{
-  padding-inline-start: 0;
-}
-
 .img-span-li{
-  max-width: 100%;
+  width: 100%;
   height: auto;
   display: flex;
-  flex-wrap: wrap;
+  flex-wrap: nowrap;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
   padding: 1rem;
-  border: 1px solid white;
 }
-
 
 .img-span-li:nth-child(odd) .text-div{
     order: 1;
@@ -106,10 +97,9 @@ ul{
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  text-align: left;
-  padding: 1rem;
-
 }
+
+
 
 .picture-title{
   margin-bottom: 1rem;
@@ -120,7 +110,6 @@ ul{
 .picture-description{
   font-size: 1.2rem;
   max-width: 80%;
-  text-align: left;
 }
 
 .img-wrapper{
@@ -129,42 +118,39 @@ ul{
 
 
 img{
-    max-width: 100%;
-    height: auto;
-    border-radius: 30px;
-    filter: drop-shadow(2px 2px 2px white);
-    padding: 1rem;
-    transition: all 0.5s ease-in;
+  max-width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    object-fit: cover;
+    border-top-right-radius: 30px;
+    border-top-left-radius: 30px;
+    object-position: center;
 }
 
-/* Tablet Styles */
-@media screen and (min-width: 768px) and (max-width: 1300px) {
 
-/* main title */
+@media screen and (max-width:1300px){
 
 .main-title{
-    width: 100vw;
-    height: 90vh;
+  width: 100vw;
+    height: 80vh;
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: space-around;
-    margin: 0rem auto;
+    justify-content: center;
+    margin: 0 auto;
+    text-align: center;
+    padding: 1rem;
+    border: none;
 }
 
 h1{
-    font-size: 4rem;
-    display: block;
-    margin: 0 auto;
-    padding: 1rem;
-    text-align: center;
+    font-size: 2rem;
 }
 
-/* gallery */
-
-.gallery{
-    max-width: 90vw;
+.grid{
+    width: 100vw;
     height: auto;
+    border: none;
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -173,8 +159,8 @@ h1{
 }
 
 
-main > ul{
-    max-width: 90%;
+.grid > ul{
+    width: 100%;
     height: auto;
     list-style-type: none;
     display: flex;
@@ -186,166 +172,49 @@ main > ul{
 }
 
 .img-span-li{
-  max-width: 80%;
-  height: auto;
-  display: flex;
-  flex-wrap: wrap;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  padding: 1rem;
-  border: 1px solid black;
-  margin: 2rem auto;
+        max-width: 100%;
+        height: auto;
+        display: flex;
+        flex-wrap: nowrap;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-between;
+        padding: 1rem;
+        border: 1px solid black;
+        margin: 2rem 0rem;
+        border-top-right-radius: 30px;
+        border-top-left-radius: 30px;
 }
 
-
 .img-span-li:nth-child(odd) .text-div{
-    order: 1;
+    order: 1 !important;
 }
 
 .img-span-li:nth-child(even) .text-div{
-    order: 2;
+    order: 1 !important;
 }
 
 .text-div{
-  max-width: 50%;
-  height: 100%;
+  max-width: 100%;
+  height: auto;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  text-align: left;
   padding: 1rem;
-
 }
+
+
 
 .picture-title{
   margin-bottom: 1rem;
   text-decoration: underline;
   font-size: 1.5rem;
-  flex-wrap: nowrap;
 }
 
 .picture-description{
   font-size: 1rem;
   max-width: 100%;
-  text-align: left;
-}
-
-.img-wrapper{
-  max-width: 50%;
-}
-
-
-img{
-    max-width: 100%;
-    height: auto;
-    border-radius: 30px;
-    filter: drop-shadow(2px 2px 2px white);
-    padding: 1rem;
-    transition: all 0.5s ease-in;
-}
-
-}
-
-
-/* Mobile Styles */
-@media screen and (max-width: 767px) {
-
-/* main title */
-
-.main-title{
-    width: 100vw;
-    height: 90vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-around;
-    margin: 0rem auto;
-}
-
-h1{
-    font-size: 3rem;
-    display: block;
-    margin: 0 auto;
-    padding: 1rem;
-    text-align: center;
-}
-
-/* gallery */
-
-.gallery{
-    max-width: 100vw;
-    height: auto;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-around;
-    margin: 0 auto;
-}
-
-
-main > ul{
-    max-width: 100%;
-    height: auto;
-    list-style-type: none;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    margin: 0 auto;
-    padding: 1rem;
-}
-
-.img-span-li{
-  max-width: 100%;
-  height: auto;
-  display: flex;
-  flex-wrap: nowrap;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  padding: 1rem;
-  border: 1px solid black;
-  margin: 2rem 1rem;
-  border-top-right-radius: 30px;
-  border-top-left-radius: 30px;
-
-}
-
-
-.img-span-li:nth-child(odd) .text-div{
-    order: 1;
-}
-
-.img-span-li:nth-child(even) .text-div{
-    order: 2;
-}
-
-.text-div{
-  max-width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  text-align: left;
-  padding: 1rem;
-  margin: 0 auto;
-
-}
-
-.picture-title{
-  margin-bottom: 1rem;
-  text-decoration: underline;
-  font-size: 1.5rem;
-  flex-wrap: nowrap;
-}
-
-.picture-description{
-  font-size: 1rem;
-  max-width: 100%;
-  text-align: left;
 }
 
 .img-wrapper{
@@ -354,14 +223,14 @@ main > ul{
 
 
 img{
-    max-width: 100%;
-    height: auto;
-    border-radius: 30px;
-    filter: drop-shadow(2px 2px 2px white);
-    padding: 1rem;
-    transition: all 0.5s ease-in;
+  max-width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    object-fit: cover;
+    border-top-right-radius: 30px;
+    border-top-left-radius: 30px;
+    object-position: center;
 }
-
 }
 
 </style>
